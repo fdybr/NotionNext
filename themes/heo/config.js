b63d7fbfdb3204e6c4a7a465b50aff2c9c43851c
fdybr <53993412+fdybr@users.noreply.github.com>
2025-08-12 20:11:22 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,7 +8,7 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '月が綺麗ですね', url: 'https://notion-next-ivory-seven-62.vercel.app/' },
+    { title: '国境の長いトンネルを抜けると雪国であった', url: 'https://notion-next-ivory-seven-62.vercel.app/' },
     { title: '月が綺麗ですね', url: 'https://notion-next-ivory-seven-62.vercel.app/' }
   ],
 
