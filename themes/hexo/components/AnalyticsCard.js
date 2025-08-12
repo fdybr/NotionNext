fd3e4c1641afbf87782aeb7f9496b6c1534bdb4a
fdybr <53993412+fdybr@users.noreply.github.com>
2025-08-12 21:44:30 +0800

    Update AnalyticsCard.js

--- themes/hexo/components/AnalyticsCard.js
+++ themes/hexo/components/AnalyticsCard.js
@@ -1,30 +1,48 @@
-import Card from './Card'
+import CONFIG from '../config'
+import { siteConfig } from '@/lib/config'
 
-export function AnalyticsCard (props) {
+/**
+ * 博客统计卡牌
+ * @param {*} props
+ * @returns
+ */
+export function AnalyticsCard(props) {
+  const targetDate = new Date(siteConfig('HEO_SITE_CREATE_TIME', null, CONFIG))
+  const today = new Date()
+  const diffTime = today.getTime() - targetDate.getTime() // 获取两个日期之间的毫秒数差值
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)) // 将毫秒数差值转换为天数差值
+  const postCountTitle = siteConfig('HEO_POST_COUNT_TITLE', null, CONFIG)
+  const siteTimeTitle = siteConfig('HEO_SITE_TIME_TITLE', null, CONFIG)
+  const siteVisitTitle = siteConfig('HEO_SITE_VISIT_TITLE', null, CONFIG)
+  const siteVisitorTitle = siteConfig('HEO_SITE_VISITOR_TITLE', null, CONFIG)
+
   const { postCount } = props
-  return <Card>
-    <div className='ml-2 mb-3 '>
-      <i className='fas fa-chart-area' /> 统计
-    </div>
-    <div className='text-xs  font-light justify-center mx-7'>
-      <div className='inline'>
-        <div className='flex justify-between'>
-          <div>文章数:</div>
-          <div>{postCount}</div>
+  return <>
+        <div className='text-md flex flex-col space-y-1 justify-center px-3'>
+            <div className='inline'>
+                <div className='flex justify-between'>
+                    <div>{postCountTitle}</div>
+                    <div>{postCount}</div>
+                </div>
+            </div>
+            <div className='inline'>
+                <div className='flex justify-between'>
+                    <div>{siteTimeTitle}</div>
+                    <div>{diffDays} 天</div>
+                </div>
+            </div>
+            <div className='hidden busuanzi_container_page_pv'>
+                <div className='flex justify-between'>
+                    <div>{siteVisitTitle}</div>
+                    <div className='busuanzi_value_page_pv' />
+                </div>
+            </div>
+            <div className='hidden busuanzi_container_site_uv'>
+                <div className='flex justify-between'>
+                    <div>{siteVisitorTitle}</div>
+                    <div className='busuanzi_value_site_uv' />
+                </div>
+            </div>
         </div>
-      </div>
-      <div className='hidden busuanzi_container_page_pv ml-2'>
-        <div className='flex justify-between'>
-          <div>访问量:</div>
-          <div className='busuanzi_value_page_pv' />
-        </div>
-      </div>
-      <div className='hidden busuanzi_container_site_uv ml-2'>
-        <div className='flex justify-between'>
-          <div>访客数:</div>
-          <div className='busuanzi_value_site_uv' />
-        </div>
-      </div>
-    </div>
-  </Card>
+        </>
 }
